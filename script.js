028b7930ba1f30b2dc227397e8f4f69d443959a1
rupeshsingh17889-gif <rupeshsingh17889@gmail.com>
2025-11-27 13:57:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    document.getElementById("navbar-mobile").style.left="0px"
+}
+
+function closeMenu(){
+    document.getElementById("navbar-mobile").style.left="-280px"
+}
